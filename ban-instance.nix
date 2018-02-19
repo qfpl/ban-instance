774eb578dfa853f42580a5121bff59548b3abac6
Jack Kelly <jack.kelly@data61.csiro.au>
2018-02-19 14:50:15 +1100

    Add nix files

---
+++ ban-instance.nix
@@ -0,0 +1,11 @@
+{ mkDerivation, base, stdenv, template-haskell }:
+mkDerivation {
+  pname = "ban-instance";
+  version = "0.1.0.0";
+  src = ./.;
+  libraryHaskellDepends = [ base template-haskell ];
+  testHaskellDepends = [ base ];
+  homepage = "https://github.com/githubuser/ban-instance#readme";
+  description = "For when a type should never be an instance of a class";
+  license = stdenv.lib.licenses.bsd3;
+}
---
+++ default.nix
@@ -0,0 +1,12 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+  inherit (nixpkgs) pkgs;
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  ban-instance = haskellPackages.callPackage ./ban-instance.nix {};
+in
+  ban-instance
